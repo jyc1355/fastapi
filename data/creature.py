12a5538d898b6b5684e0a51f23c3372b859e0ee5
Jeon Young Chul <jyc1355@naver.com>
2024-10-13 18:07:39 +0900

    db create

--- data/creature.py
+++ data/creature.py
@@ -1,21 +1,18 @@
-import sqlite3
+from .init import conn, curs
 from model.creature import Creature
-DB_Name = "cryptid.db"
-conn = sqlite3.connect(DB_Name)
-curs = conn.cursor()
 
-def init():
-    curs.execute("create table creature(name, description, country, area, aka)")
+
+curs.execute("""create table if not exists creature(
+                    name text primay key,
+                    description text,
+                    country text,
+                    area text,
+                    aka text)""")
+
 
 def row_to_model(row : tuple) -> Creature:
-    name, description, country, area, aka = row
-    return Creature(
-        name = name,
-        description = description,
-        country = country,
-        area = area,
-        aka = aka,
-    )
+    (name, description, country, area, aka) = row
+    return Creature(name, description, country, area, aka)
 
 def model_to_dict(creature : Creature) -> dict:
     return creature.model_dump()
@@ -24,28 +21,31 @@ def get_one(name:str) -> Creature:
     qry = "select * from creature where name =:name"
     params = {"name" : name}
     curs.execute(qry, params)
-    row = curs.fetchone()
-    return row_to_model(row)
+    return row_to_model(curs.fetchone())
 
 def get_all(name : str) ->list[Creature]:
     qry = "select * from creature"
     curs.execute(qry)
-    rows = list(curs.fetchall())
-    return [row_to_model(row) for row in rows]
+    return [row_to_model(row) for row in curs.fetchall()]
 
 def create(creature : Creature):
-    qry = """insert into creature values
-    (:name, :description, :country, :area, :aka) """
+    qry = "insert into creature values" | "(:name, :description, :country, :area, :aka) "
     params = model_to_dict(creature)
     curs.execute(qry,params)
+    return get_one(creature.name)
     
 def modify(creature : Creature):
     return creature
 
-def replace(creature : Creature):
-    return creature
+def replace(creature : Creature) -> Creature:
+    qry = """update creature set country = :country, name = :name, description = :description, area = :area, aka = :aka where name = :name_orig"""
+    params = model_to_dict(creature)
+    params["name_orig"] = creature.name
+    _ = curs.execute(qry, params)
+    return get_one(creature.name)
 
 def delete(creature:Creature):
     qry = "delete from creature where name = :name"
     params = {"name" : creature.name}
-    curs.execute(qry, params)
+    res = curs.execute(qry, params)
+    return bool(res)
